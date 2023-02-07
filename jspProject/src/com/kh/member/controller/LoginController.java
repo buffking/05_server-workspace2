f28423f31e484ffa2700f834e4aab9e90985bb0a
buffking <ymjuneseok@naver.com>
2023-02-07 12:30:18 +0900

    커밋
    커밋

--- jspProject/src/com/kh/member/controller/LoginController.java
+++ jspProject/src/com/kh/member/controller/LoginController.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.kh.member.model.service.MemberService;
+import com.kh.member.model.vo.Member;
+
 /**
  * Servlet implementation class LoginController
  */
@@ -27,11 +30,16 @@ public class LoginController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 1) 전달값에 한글이 있을 경우 인코딩 처리해야됨 (post 방식에만)
+		request.setCharacterEncoding("UTF-8");
 		
 		// 2) 요청시 전달값 뽑아서 변수 또는 객체에 기록하기
+		String userId = request.getParameter("userId");
+		String userPwd = request.getParameter("userPwd");
 		
 		// 3) 요청처리 (db에 sql문 실행)
 		//	  해당 요청을 처리하는 서비스 클래스의 메소드 호출 및 결과 보기
+		Member loginUser = new MemberService().loginMember(userId, userPwd); // null 혹은 로그인한 멤버객체 담김
+		System.out.println(loginUser);
 		
 		// 4) 처리된 결과를 가지고 사용자가 보게될 응답뷰(jsp) 지정 후 포워딩 또는 url 재요청
 		
