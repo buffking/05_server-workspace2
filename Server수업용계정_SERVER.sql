aef5a52ffd3ad8eb322320039012041c321849ea
buffking <ymjuneseok@naver.com>
2023-02-10 15:16:31 +0900

    커밋
    커밋

--- Server수업용계정_SERVER.sql
+++ Server수업용계정_SERVER.sql
@@ -1,1 +1,8 @@
-SELECT * FROM MEMBER;
+UPDATE MEMBER
+    SET USER_NAME = ?
+      , PHONE = ?
+      , EMAIL = ?
+      , ADDRESS = ?
+      , INTEREST = ?
+      , MODIFY_DATE = SYSDATE
+WHERE   USER_ID = ?
