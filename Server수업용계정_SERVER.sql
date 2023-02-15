3d4d91f395c9bae4d6e46b2e7cb558a5231e31f4
buffking <ymjuneseok@naver.com>
2023-02-15 15:14:18 +0900

    커밋
    커밋

--- Server수업용계정_SERVER.sql
+++ Server수업용계정_SERVER.sql
@@ -1,9 +1,20 @@
-SELECT
-       NOTICE_NO
-     , NOTICE_TITLE
-     , NOTICE_CONTENT
-     , USER_ID
-     , CREATE_DATE
-  FROM NOTICE
-  JOIN MEMBER ON (NOTICE_WRITER = USER_NO)
-  WHERE NOTICE_NO = ?
+SELECT *
+    FROM (SELECT ROWNUM RNUM, A.*
+          FROM (
+        SELECT
+               BOARD_NO
+             , CATEGORY_NAME
+             , BOARD_TITLE
+             , USER_ID
+             , COUNT
+             , TO_CHAR(CREATE_DATE, 'YYYY/MM/DD') "CREATE_DATE"
+          FROM BOARD B
+          JOIN CATEGORY USING(CATEGORY_NO)
+          JOIN MEMBER ON (BOARD_WRITER = USER_NO)
+         WHERE BOARD_TYPE = 1
+           AND B.STATUS = 'Y'
+         ORDER
+            BY BOARD_NO DESC
+             ) A
+         )
+         WHERE RNUM BETWEEN ? AND ?
